3d5a6abd546ea279c27fa1700c50ce17afcb2be5
Diego <diegosantos4957@gmail.com>
2025-12-12 18:45:11 -0300

    refactor: remove class test_class

--- test_class.py
+++
@@ -1,9 +0,0 @@
-class TestPrivateMethod:
-    def __init__(self):
-        self.__private_value = 42
-
-    def _private_method(self):
-        return self.__private_value
-
-    def public_method(self):
-        return self.__private_value
