b2006f0e459bde57526c2503cb3ff8d57b72eb72
Diego <diegosantos4957@gmail.com>
2025-12-13 00:24:13 -0300

    feat: add is_bsl_a function for Brazilian Sign Language detection

---
+++ bsl/is_validated/is_bsl_a.py
@@ -0,0 +1,26 @@
+from bsl.validated import lm_to_point, distance
+
+WRIST = 0
+THUMB_TIP = 4
+INDEX_MCP = 5
+FINGER_TIPS = [8, 12, 16, 20]
+
+def is_bsl_a(landmarks, w, h, thumb_index_thresh=0.12, finger_fold_thresh=0.12):
+    try:
+        min_side = min(w, h)
+
+        wrist = lm_to_point(landmarks.landmark[WRIST], w, h)
+        thumb = lm_to_point(landmarks.landmark[THUMB_TIP], w, h)
+        index_mcp = lm_to_point(landmarks.landmark[INDEX_MCP], w, h)
+
+        for i in FINGER_TIPS:
+            tip = lm_to_point(landmarks.landmark[i], w, h)
+            if distance(tip, wrist) > min_side + finger_fold_thresh:
+                return False
+
+        if distance(thumb, index_mcp) > min_side + thumb_index_thresh:
+            return False
+
+        return True
+    except Exception:
+        return False
